462581adef6f9adad9566aacc95547de91e1d6d3
Nikita Karamov <nick@karamoff.dev>
2020-11-28 23:34:13 +0100

    Update install script

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-echo "Installing a systemd service..."
+echo "Installing dependencies..."
+poetry install
+
+echo "Installing the systemd service..."
 
 echo "[Unit]
 Description=Start nick-raspi-01-bot
@@ -14,4 +17,11 @@ WorkingDirectory=$(pwd)
 [Install]
 WantedBy=multi-user.target" > /etc/systemd/system/nick-raspi-01-bot.service
 
+echo "Enabling the systemd service..."
+systemctl enable nick-raspi-01-bot
+
+echo "Starting the systemd service..."
 systemctl start nick-raspi-01-bot
+
+echo "Installation complete!"
+echo "Type 'sudo journalctl -u nick-raspi-01-bot' to see the logs"
