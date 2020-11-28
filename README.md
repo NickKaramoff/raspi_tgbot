3eab82910ff0defd916b74333bdf49d5a1465f3f
Nikita Karamov <nick@karamoff.dev>
2020-11-29 00:02:07 +0100

    Split install into two scripts

--- README.md
+++ README.md
@@ -8,4 +8,5 @@ Telegram Bot for my Raspberry Pi
 git clone git@github.com:NickKaramoff/nick-raspi-01-bot.git
 cd nick-raspi-01-bot
 ./install.sh
+sudo ./install-systemd.sh
 ```
--- install.sh
+++ install.sh
@@ -1,30 +1,4 @@
-#!/bin/sh
-
 /home/nick/.poetry/bin/poetry env use /home/nick/.pyenv/shims/python3
 
 echo "Installing dependencies..."
 /home/nick/.poetry/bin/poetry install --no-dev
-
-echo "Installing the systemd service..."
-
-sudo echo "[Unit]
-Description=Start nick-raspi-01-bot
-
-[Service]
-Type=simple
-WorkingDirectory=$(pwd)
-User=nick
-ExecStart=/home/nick/.poetry/bin/poetry run raspi-bot
-Restart=on-failure
-
-[Install]
-WantedBy=multi-user.target" > /etc/systemd/system/nick-raspi-01-bot.service
-
-echo "Enabling the systemd service..."
-sudo systemctl enable nick-raspi-01-bot
-
-echo "Starting the systemd service..."
-sudo systemctl start nick-raspi-01-bot
-
-echo "Installation complete!"
-echo "Type 'sudo journalctl -u nick-raspi-01-bot' to see the logs"
