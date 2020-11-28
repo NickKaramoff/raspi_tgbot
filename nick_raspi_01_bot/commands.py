6af3e9d7ac626777d4251003e28954ec0dc50ca6
Nikita Karamov <nick@karamoff.dev>
2020-11-28 20:30:37 +0100

    Update /start command

--- nick_raspi_01_bot/commands.py
+++ nick_raspi_01_bot/commands.py
@@ -1,2 +1,12 @@
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome")
+from telegram import Update
+from telegram.ext import CallbackContext
+
+from .util.allower import allowed_only
+
+
+@allowed_only
+def start(update: Update, context: CallbackContext):
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=f"Welcome home, {update.effective_user.first_name}",
+    )
