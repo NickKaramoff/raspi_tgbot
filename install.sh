59fcdb0acfdb324d29baad17acf35871a8f05322
Nikita Karamov <nick@karamoff.dev>
2020-11-29 00:00:12 +0100

    Fix install script 2

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ echo "Installing dependencies..."
 
 echo "Installing the systemd service..."
 
-echo "[Unit]
+sudo echo "[Unit]
 Description=Start nick-raspi-01-bot
 
 [Service]
@@ -21,10 +21,10 @@ Restart=on-failure
 WantedBy=multi-user.target" > /etc/systemd/system/nick-raspi-01-bot.service
 
 echo "Enabling the systemd service..."
-systemctl enable nick-raspi-01-bot
+sudo systemctl enable nick-raspi-01-bot
 
 echo "Starting the systemd service..."
-systemctl start nick-raspi-01-bot
+sudo systemctl start nick-raspi-01-bot
 
 echo "Installation complete!"
 echo "Type 'sudo journalctl -u nick-raspi-01-bot' to see the logs"
